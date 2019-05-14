cacff92ca8f08b64abfa527d9c3207dcc7fedde5
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-05-14 16:11:02 +0300

    connect to the mongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+const encodedPass = "596125%2Fserg";
+module.exports = {
+  mongoURI: `mongodb+srv://serhii:${encodedPass}@cluster0-tczzi.mongodb.net/test?retryWrites=true`
+};
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello Server!!!");
 });
+
+// DB config
+const db = require("./config/keys");
+
+// connect to the mongoDB
+mongoose
+  .connect(db.mongoURI, { useNewUrlParser: true })
+  .then(() => console.info("MongoDB connected"))
+  .catch(ex => console.error(ex));
 
 app.listen(port, () => console.log(`Server is running on ${port} port`));
