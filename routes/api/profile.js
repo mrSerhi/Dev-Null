463f74f2f9803ec415bdb87c2600a7b73a83b0e7
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-05-17 20:27:53 +0300

    create privat current profile api route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// include Profile model
+const Profile = require("../../models/Profile");
+// include User model
+const User = require("../../models/User");
 
 // @route full: /api/profile/test
 // @access: Public
@@ -7,5 +14,27 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "Profile route is available" });
 });
+
+// @route-full: /api/profile/
+// @access: Privat
+// @desc:  Get/check Current Profile of User
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // will be used like a storage of errors
+    const error = {};
+    //
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        error.noprofile = "User don't has a profile yet";
+        // check on profile exist
+        if (!profile) return res.status(404).json(error);
+
+        res.json(profile);
+      })
+      .catch(ex => res.status(404).json(ex));
+  }
+);
 
 module.exports = router;
