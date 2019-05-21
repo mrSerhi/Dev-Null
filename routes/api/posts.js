3f2f9ac37da96026e3186a8fe33402c3885b9c91
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-05-21 15:25:06 +0300

    GET public api route for getting a post on post ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,6 +35,23 @@ router.get("/", (req, res) => {
     .catch(ex => res.status(404).json(ex.message));
 });
 
+// @route-full: GET /api/posts/:post_id
+// @access: Public
+// @desc:  Get post by post_id
+router.get("/:post_id", (req, res) => {
+  Post.findOne({ _id: req.params.post_id })
+    .then(post => {
+      if (!post) return res.status(404).json({ msg: "Post not found" });
+
+      res.json(post);
+    })
+    .catch(() =>
+      res
+        .status(404)
+        .json({ msg: `Not found post with ${req.params.post_id} ID` })
+    );
+});
+
 // @route-full: POST /api/posts
 // @access: Privat
 // @desc:  Create post
