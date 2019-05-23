aaca5cc7d3f43c21d79bc4ee1910247c9f0477f8
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-05-23 15:35:15 +0300

    add bootstrap && font-awesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"; //global bootsrap css styles
+
+// include font-awesome root conf with nums of icons
+import "./fontAwesome";
 
 function App() {
   return (
     <div className="App">
-      <h1>DevNull project start at now</h1>
+      <h1 className="display-3 text-center">
+        <FontAwesomeIcon icon="check-square" color="yellow" />
+        DevNull project start at now
+        <FontAwesomeIcon icon="coffee" rotation={90} />
+      </h1>
     </div>
   );
 }
