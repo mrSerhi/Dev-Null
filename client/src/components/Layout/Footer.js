63991671fd96930a40ccee99b3f377fa293088fb
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-05-23 16:55:57 +0300

    fix: add styles to fixed footer to bottom

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+const styles = {
+  position: "fixed",
+  width: "100%",
+  bottom: 0
+};
+
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-3 text-right">
-      Copyright &copy; {new Date().getFullYear()} DevNull
-    </footer>
+    <div style={styles}>
+      <footer className="bg-dark text-white mt-5 p-3 text-right navbar-fixed-bottom">
+        <div className="container">
+          Copyright &copy; {new Date().getFullYear()} DevNull
+        </div>
+      </footer>
+    </div>
   );
 };
 
