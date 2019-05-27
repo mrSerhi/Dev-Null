564b53c2fca73aad2bd51dbe7aee936a08e95758
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-05-27 20:43:01 +0300

    fix: if jwt token is exists in localStorage, when user passed auth and has access everywere. next -> add case: log out

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./assets/utils/setAuthToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
+// styles
 import "./App.css";
 
 // include font-awesome root conf with nums of icons
@@ -13,6 +18,22 @@ import MiddleSection from "./components/Layout/MiddleSection";
 import Footer from "./components/Layout/Footer";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
+
+// check if current user token exists in localStorage.
+// the User who passed auth verification has access to change profile and etc...
+if (localStorage.jwtToken) {
+  // decoded token
+  const userData = jwt_decode(localStorage.jwtToken);
+
+  // set Auth-header
+  setAuthToken(localStorage.jwtToken);
+
+  // set user and auth in store.auth
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: userData
+  });
+}
 
 function App() {
   return (
