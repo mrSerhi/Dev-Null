793890a8144bd06c11051674b2f6aae4876291bc
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-06-04 13:19:21 +0300

    Add handlers to AddExperience component. Next -> add logic for submiting data to the server

--- client/src/components/AdditionalToProfile/AddExperience/AddExperience.js
+++ client/src/components/AdditionalToProfile/AddExperience/AddExperience.js
@@ -28,19 +28,27 @@ class AddExperience extends Component {
     description: "",
     errors: {}
   };
-  render() {
-    const {
-      title,
-      company,
-      location,
-      from,
-      to,
-      current,
-      description,
-      errors,
-      disabled
-    } = this.state;
 
+  handeOnSubmit = e => {
+    e.preventDefault();
+
+    alert("submit");
+  };
+
+  handleOnChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  handleOnChangeFrom = data => this.setState({ from: data });
+  handleOnChangeTo = data => this.setState({ to: data });
+
+  handleOnCheck = () => {
+    this.setState(prevState => ({
+      current: !prevState.current,
+      disabled: !prevState.disabled,
+      to: ""
+    }));
+  };
+
+  render() {
     return (
       <section className="add-experience mb-5">
         <div className="container">
@@ -51,55 +59,55 @@ class AddExperience extends Component {
               <Form onSubmit={this.handeOnSubmit}>
                 <FormItem
                   name="title"
-                  value={title}
+                  value={this.state.title}
                   onChange={this.handleOnChange}
                   placeholder="*Job Title"
-                  errors={errors}
+                  errors={this.state.errors}
                 />
                 <FormItem
                   name="company"
-                  value={company}
+                  value={this.state.company}
                   onChange={this.handleOnChange}
                   placeholder="*Company Title"
-                  errors={errors}
+                  errors={this.state.errors}
                 />
                 <FormItem
                   name="location"
-                  value={location}
+                  value={this.state.location}
                   onChange={this.handleOnChange}
                   placeholder="Location"
-                  errors={errors}
+                  errors={this.state.errors}
                 />
                 <span className="mr-1">From:</span>
                 <DatePicker
-                  name="from"
-                  selected={from}
-                  onChange={this.handleOnChange}
+                  selected={this.state.from}
+                  onChange={this.handleOnChangeFrom}
                   dateFormat="dd/MM/yyyy"
                   placeholderText="dd/MM/yyyy"
                 />
                 <span className="ml-3 mr-1">To:</span>
                 <DatePicker
-                  name="to"
-                  selected={to}
-                  onChange={this.handleOnChange}
-                  disabled={disabled}
+                  selected={this.state.to}
+                  onChange={this.handleOnChangeTo}
+                  disabled={this.state.disabled}
                   dateFormat="dd/MM/yyyy"
-                  placeholderText={disabled ? "You working..." : "dd/MM/yyyy"}
+                  placeholderText={
+                    this.state.disabled ? "You working..." : "dd/MM/yyyy"
+                  }
                 />
                 <FormItemCheckbox
                   checkboxID="exp-current"
-                  value={current}
-                  checked={current}
-                  onChecked={this.handleCheckClick}
-                  label="currently working"
+                  value={this.state.current}
+                  checked={this.state.current}
+                  onChecked={this.handleOnCheck}
+                  label="currently working?"
                 />
                 <FormItemTextarea
                   name="description"
-                  value={description}
+                  value={this.state.description}
                   onChange={this.handleOnChange}
                   placeholder="Some of your responsabilities from your last job..."
-                  errors
+                  errors={this.state.errors}
                 />
                 <Button type="submit" classes="btn-info">
                   <FontAwesomeIcon icon="plus" /> Add to profile
