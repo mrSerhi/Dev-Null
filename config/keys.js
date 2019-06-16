45cb8641855408ac154f7fa76ad6acb0286f93f0
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-06-16 17:48:47 +0300

    to secure app keys

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,5 @@
-const bcript = require("bcryptjs");
-
-const solt = bcript.genSaltSync(10);
-const jwtPrivetKey = bcript.hashSync("secret596125", solt);
-
-const encodedPass = "596125%2Fserg";
-module.exports = {
-  mongoURI: `mongodb+srv://serhii:${encodedPass}@cluster0-tczzi.mongodb.net/test?retryWrites=true`,
-  jwtPrivetKey
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys-prod");
+} else {
+  module.exports = require("./keys-dev");
+}
